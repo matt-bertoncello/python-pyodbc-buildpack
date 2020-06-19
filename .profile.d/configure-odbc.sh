349a9d711149460614227a5931b42bc5e9142285
matt-bertoncello <mrbertoncello@diamond-energy.com>
2020-06-19 17:32:50 +1000

    changed directory that the driver is saved to

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -7,6 +7,6 @@ mkdir -p /etc
 
 echo "[ODBC Driver 17 for SQL Server]
 Description=Microsoft ODBC Driver 17 for SQL Server
-Driver=/app/.apt/usr/lib/libmsodbcsql-17.1.so.0.1
+Driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.5.so.2.1
 UsageCount=1
 " > ${ODBCSYSINI}/odbc.ini
