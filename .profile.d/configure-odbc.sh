8bdec12035d3c413ef73fd8d26136df3c4274def
matt-bertoncello <mrbertoncello@diamond-energy.com>
2020-06-19 17:45:12 +1000

    make directory before copying driver

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -4,9 +4,10 @@
 export ODBCSYSINI=${HOME}/.apt/usr/lib/odbc/conf/
 
 mkdir -p /etc
+mkdir -p /opt/microsoft/msodbcsql17/lib64/
 
 echo "[ODBC Driver 17 for SQL Server]
 Description=Microsoft ODBC Driver 17 for SQL Server
-Driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.5.so.2.1
+Driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.1.so.0.1
 UsageCount=1
 " > ${ODBCSYSINI}/odbc.ini
