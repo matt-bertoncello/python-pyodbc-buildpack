5e387d51165a4ecf42a4665b37c294e5d43d7265
b74107 <115028865+b74107@users.noreply.github.com>
2023-03-14 09:51:38 -0600

    Update for Heroku stack 22
    Update ODBC driver to 17.10 complied on Ubuntu 22.04 LTS to

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -7,6 +7,6 @@ mkdir -p /etc
 
 echo "[ODBC Driver 17 for SQL Server]
 Description=Microsoft ODBC Driver 17 for SQL Server
-Driver=/app/.apt/usr/lib/libmsodbcsql-17.5.so.2.1
+Driver=/app/.apt/usr/lib/libmsodbcsql-17.10.so.2.1
 UsageCount=1
 " > ${ODBCSYSINI}/odbc.ini
